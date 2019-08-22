fa4d629c7f3247db409a8ea276b5d8aacb8d2fc9
John Schmidley <tlg8n@outlook.com>
2019-08-22 22:28:40 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,31 @@
 def square_array(array)
-  # your code here
+  
+  counter = 0
+
+ new_array = []
+while array[counter] do
+  new_array << array[counter] **2 
+  counter += 1
+
 end
+new_array
+end
+
+
+# describe '#square_array' do
+
+#   it 'does not call on each, collect, map, or inject' do
+#     numbers = [1,2,3]
+#     expect(numbers).to_not receive(:each)
+#     expect(numbers).to_not receive(:collect)
+#     expect(numbers).to_not receive(:map)
+#     expect(numbers).to_not receive(:inject)
+#     square_array(numbers)
+#   end
+
+#   it 'should square the elements in an array' do
+#     expect(square_array([1,2,3])).to eq([1,4,9])
+#     expect(square_array([9,10,16,25])).to eq([81,100,256,625])
+#   end
+
+# end
